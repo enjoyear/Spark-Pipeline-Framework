f4cdba75f57ad47ff58137473c0ffd07a17db526
Chen Guo <almightgc@gmail.com>
2017-07-12 16:06:42 -0700

    Add spark-common

--- spark-ingestion/src/main/scala/com/chen/guo/HelloWorld.scala
+++ spark-ingestion/src/main/scala/com/chen/guo/HelloWorld.scala
@@ -1,5 +1,13 @@
 package com.chen.guo
 
+import java.io.File
+
+import com.typesafe.config.{Config, ConfigFactory}
+
+
 object HelloWorld extends App {
-  println("abc")
+  private val config: Config = ConfigFactory.parseFile(new File("/Users/chguo/repos/enjoyear/Spark-Pipeline-Framework/spark-ingestion/src/main/resources/com/chen/guo/simple.conf"))
+
+  println(config.getString("Hi"))
+
 }
