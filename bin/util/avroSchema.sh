d0d027f4b61a5871960189a1885a30d57ed130d4
Chen Guo <almightgc@gmail.com>
2017-07-17 13:47:09 -0700

    add avro util scripts.

---
+++ bin/util/avroSchema.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+if [ ! -f ${BIN_DIR}/jars/avro-tools-1.8.1.jar ]; then
+   cd $BIN_DIR/jars/ && curl -O http://central.maven.org/maven2/org/apache/avro/avro-tools/1.8.1/avro-tools-1.8.1.jar
+fi
+
+java -jar ${BIN_DIR}/jars/avro-tools-1.8.1.jar getschema $1
