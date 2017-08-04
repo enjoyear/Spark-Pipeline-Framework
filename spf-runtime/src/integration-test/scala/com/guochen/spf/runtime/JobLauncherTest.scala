40223f409c4f479931bb1b1390ccb84043896b23
Chen Guo <almightgc@gmail.com>
2017-08-04 15:58:45 -0700

    move test files under test sources

--- spf-runtime/src/integration-test/scala/com/guochen/spf/runtime/JobLauncherTest.scala
+++ spf-runtime/src/integration-test/scala/com/guochen/spf/runtime/JobLauncherTest.scala
@@ -1,17 +1,17 @@
-package com.guochen.spf
+package com.guochen.spf.runtime
 
 import java.io.File
 
-import com.guochen.spf.runtime.JobLauncher
 import com.typesafe.config.ConfigFactory
 import org.junit.runner.RunWith
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class SampleScalaIntegrationTest extends FlatSpec with Matchers {
+class JobLauncherTest extends FlatSpec with Matchers {
   "SampleScalaIntegrationTest" should "be executed" in {
-    val config = ConfigFactory.parseFile(new File("/Users/chguo/repos/enjoyear/Spark-Pipeline-Framework/examples/ingestion/simple/file-ingestion.conf"))
+    val config = ConfigFactory.parseFile(
+      new File(getClass.getResource("file-ingestion.conf").getPath))
 
     new JobLauncher(config).start()
   }
