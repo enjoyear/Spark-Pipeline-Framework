c41882faa6a8977acca92074368dffde1d4ead5f
Chen Guo <almightgc@gmail.com>
2017-08-18 19:41:04 -0700

    Working on the twitter example

--- test-spark-kafka/src/main/scala/fun/SparkStreamingExample.scala
+++ test-spark-kafka/src/main/scala/fun/SparkStreamingExample.scala
@@ -4,16 +4,16 @@ import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /**
-  * Server: nc -l 3333
-  * Client: nc localhost 3333
+  * To run this on your local machine, you need to first run a Netcat server
+  * Server: nc -lk 9999
   */
 object SparkStreamingExample extends App {
   val sparkConf = new SparkConf().setMaster("local[4]").setAppName("NetworkWordCount")
-  val ssc = new StreamingContext(sparkConf, Seconds(2))
+  val ssc = new StreamingContext(sparkConf, Seconds(10))
 
   // Create a DStream that will connect to localhost:9999
-  // Establish a socket stream on top of D-Streams that reads line by line, every two seconds.
-  val lines = ssc.socketTextStream("localhost", 3333)
+  // Establish a socket stream on top of D-Streams that reads line by line, every 10 seconds.
+  val lines = ssc.socketTextStream("localhost", 9999)
 
   val words = lines.flatMap(_.split(" "))
   val pairs = words.map(word => (word, 1))
--- test-spark-kafka/src/main/scala/fun/SparkTwitterStreaming.scala
+++ test-spark-kafka/src/main/scala/fun/SparkTwitterStreaming.scala
@@ -2,24 +2,56 @@ package fun
 
 import java.util.Properties
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
 
 object SparkTwitterStreaming extends App {
   private val props = new Properties
   private val configStream = getClass.getResourceAsStream("/twitter.conf")
   props.load(configStream)
-
   println(props)
 
-  private val builder = SparkSession.builder().master("local").appName("twitter-streaming")
-  private val session = builder.getOrCreate()
-
-  // Set the system properties so that Twitter4j library used by twitter stream
-  // can use them to generat OAuth credentials
+  // Set the system properties for twitter stream
   System.setProperty("twitter4j.oauth.consumerKey", props.getProperty("consumerKey"))
   System.setProperty("twitter4j.oauth.consumerSecret", props.getProperty("consumerSecret"))
   System.setProperty("twitter4j.oauth.accessToken", props.getProperty("accessToken"))
   System.setProperty("twitter4j.oauth.accessTokenSecret", props.getProperty("accessTokenSecret"))
 
-  session
+  // Make sure to set master to local mode with at least 2 threads
+  // 1 thread is used for collecting the incoming streams
+  // another thread for processing it
+  val sparkConf = new SparkConf().setMaster("local[4]").setAppName("TwitterStream")
+  // Set the Spark StreamingContext to create a DStream for every 10 seconds
+  val ssc = new StreamingContext(sparkConf, Seconds(10))
+
+  val stream = TwitterUtils.createStream(ssc, None, Seq("hi"))
+  val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+
+  // Get the top hashtags over the previous 60 sec window
+  // The default slideDuration is the StreamContext's batch duration
+  val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+    .map { case (topic, count) => (count, topic) }
+    .transform(_.sortByKey(ascending = false))
+
+  // Get the top hashtags over the previous 10 sec window
+  val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+    .map { case (topic, count) => (count, topic) }
+    .transform(_.sortByKey(ascending = false))
+
+  stream.print()
+  topCounts60.foreachRDD(rdd => {
+    val topList = rdd.take(10)
+    println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+    topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+  })
+  topCounts10.foreachRDD(rdd => {
+    val topList = rdd.take(10)
+    println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
+    topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+  })
+
+  ssc.start()
+  ssc.awaitTermination()
 }
