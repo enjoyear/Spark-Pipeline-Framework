d9cfd22ea4f9f41b0ebc8b8ba1cff582f0b08c27
Chen Guo <almightgc@gmail.com>
2017-08-27 11:40:34 -0700

    before upgrading kafka api

--- test-spark-kafka/src/main/java/fun/KafkaConsumer.java
+++ test-spark-kafka/src/main/java/fun/KafkaConsumer.java
@@ -15,22 +15,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * Example of creating a High Level Consumer
+ * Example of a High Level Consumer
  * <p>
- * Configuration details can be found at http://kafka.apache.org/08/documentation/#configuration
+ * How to:
+ * 1. Start zookeeper locally: bin/zookeeper-server-start.sh config/zookeeper.properties
+ * Make sure port number is 2181
+ * 2. Create a kafka topic: bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test-topic
+ * 3. Start a kafka producer: bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test-topic
  */
 public class KafkaConsumer {
   public static void main(String[] args) throws Exception {
-    Properties props = new Properties();
-    //Kafka uses ZooKeeper to store offsets of messages consumed for a specific topic and partition by this Consumer Group
-    props.put("zookeeper.connect", "localhost:2181");
-    props.put("group.id", "my-test-group");
-    props.put("zookeeper.session.timeout.ms", "400");
-    //zookeeper.sync.time is milliseconds a ZooKeeper ‘follower’ can be behind the master before an error occurs
-    props.put("zookeeper.sync.time.ms", "200");
-    //how often updates to the consumed offsets are written to ZooKeeper. Note that since the commit frequency is time based instead of # of messages consumed, if an error occurs between updates to ZooKeeper on restart you will get replayed messages.
-    props.put("auto.commit.interval.ms", "1000");
-    ConsumerConfig consumerConfig = new ConsumerConfig(props);
+    Properties kafkaConf = getProperties();
+
+    ConsumerConfig consumerConfig = new ConsumerConfig(kafkaConf);
 
     ConsumerConnector consumer = Consumer.createJavaConsumerConnector(consumerConfig);
     Map<String, Integer> topicCountMap = new HashMap<>();
@@ -46,6 +43,22 @@ public class KafkaConsumer {
       executor.submit(new MyConsumer(stream, threadNumber++));
     }
     //consumer.shutdown();
+  }
+
+  /**
+   * Configuration ref: http://kafka.apache.org/08/documentation/#configuration
+   */
+  private static Properties getProperties() {
+    Properties props = new Properties();
+    //Kafka uses ZooKeeper to store offsets of messages consumed for a specific topic and partition by this Consumer Group
+    props.put("zookeeper.connect", "localhost:2181");
+    props.put("group.id", "my-test-group");
+    props.put("zookeeper.session.timeout.ms", "400");
+    //zookeeper.sync.time is milliseconds a ZooKeeper ‘follower’ can be behind the master before an error occurs
+    props.put("zookeeper.sync.time.ms", "200");
+    //how often updates to the consumed offsets are written to ZooKeeper. Note that since the commit frequency is time based instead of # of messages consumed, if an error occurs between updates to ZooKeeper on restart you will get replayed messages.
+    props.put("auto.commit.interval.ms", "1000");
+    return props;
   }
 }
 
